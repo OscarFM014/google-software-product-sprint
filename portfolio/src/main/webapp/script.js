19e30fc2638cf7b76069d42a38281aa28f935639
OscarFM014 <oscar.gohan2010@hotmail.com>
2021-03-04 04:21:10 +0000

    feat: Show tiem servlet alert JS

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the Licens e for the specific language governing permissions and
 // limitations under the License.
 
 /**
@@ -26,3 +26,10 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function showServerTime() {
+  const responseFromServer = await fetch('/date');
+  const textFromResponse = await responseFromServer.text();
+  console.log(textFromResponse);
+  alert(textFromResponse);
+} 
