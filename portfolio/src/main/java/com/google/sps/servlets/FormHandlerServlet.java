6f758a8d916a802afff052ce82e7f1b4961b7186
OscarFM014 <oscar.gohan2010@hotmail.com>
2021-03-06 01:25:07 +0000

    act: Viewing Logs

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -14,13 +14,10 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
-
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    String nameValue = request.getParameter("name-value");
 
     // Write the value to the response so the user can see it.
-    response.setContentType("textValue/html;");
+    response.getWriter().write(nameValue);
     /* response.sendRedirect("https://ofernandez-sps-spring21.uc.r.appspot.com/#home"); */
   }
 }
