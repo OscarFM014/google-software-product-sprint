403b3fd969725ed622268a3ab33c8f3cc2448ee1
OscarFM014 <oscar.gohan2010@hotmail.com>
2021-03-06 02:10:03 +0000

    act: Viewing Logs

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,30 +1,46 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
+/** Servlet that processes text. */
 @WebServlet("/form-handler")
-public class FormHandlerServlet extends HttpServlet {
+public final class FormHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-    // Get the value entered in the form.
-    String nameValue = request.getParameter("name-value");
-    String emailValue = request.getParameter("email-value");
-    String message = request.getParameter("message-value");
-    ArrayList<String> information=new ArrayList<String>();
-    information.add(nameValue);
-    information.add(emailValue);
-    information.add(message);
-    System.out.println(information);
-
+    // Get the input from the form.
+    String text = getParameter(request, "mensaje-input", "Hello there!");
     
-    response.getWriter().write(nameValue);
-    /* response.sendRedirect("https://ofernandez-sps-spring21.uc.r.appspot.com/#home"); */
+    // Respond with the result.
+    response.setContentType("text/html;");
+    response.getWriter().println(text);
   }
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+ 
 }
